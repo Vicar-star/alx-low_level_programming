2d350f4ee110d1ad6a0dc486da73cff646a4e294
Umeh Victor <umehvictorchukwudi@gmail.com>
2022-07-05 19:41:30 -0700

    Task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,9 +8,7 @@
 int main(void)
 {
 long int i, j, k, next;
-
 j = 1;
-
 k = 2;
 
 for (i = 1; i <= 50; ++i)
@@ -18,7 +16,6 @@ for (i = 1; i <= 50; ++i)
 if (j != 20365011074)
 {
 printf("%ld, ", j);
-}
 }
 else
 {
