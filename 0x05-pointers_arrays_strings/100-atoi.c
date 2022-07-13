ffe4caa5f77e132aa44c97c900ea1d09adc38a35
Umeh Victor <umehvictorchukwudi@gmail.com>
2022-07-12 18:46:02 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,36 @@
 #include "main.h"
 
 /**
-* _atoi - Converts a string to an integer.
-* @s: The string to be converted.
-*
-* Return: The integer value of the converted string.
+* _atoi - converts a string to an integer.
+* @s: input string.
+* Return: integer.
 */
 int _atoi(char *s)
 {
-int sign = 1;
-unsigned int num = 0;
+unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-do {
-if (*s == '-')
-sign *= -1;
+while (*(s + count) != '\0')
+{
+if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+break;
 
-else if (*s >= '0' && *s <= '9')
-num = (num * 10) + (*s - '0');
+if (*(s + count) == '-')
+pn *= -1;
 
-else if (num > 0)
-break
+if (((s + count) >= '0') && ((s + count) <= '9'))
+{
+if (size > 0)
+m *= 10;
+size++;
+}
+count++;
+}
 
-} while (*s++);
+for (i = count - size; i < count; i++)
+{
+oi = oi + ((*(s + i) - 48) * m);
+m /= 10;
+}
 
-return (num *sign);
+return (oi *pn);
 }
