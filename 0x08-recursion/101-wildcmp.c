dbe1c15792e0405d1c406917bfaf574660a74f3b
Umeh Victor <umehvictorchukwudi@gmail.com>
2022-07-20 17:02:20 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,7 +14,6 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
-
 		return (*s2 == '\0');
 	}
 
@@ -26,6 +25,5 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
-
 	return (0);
 }
